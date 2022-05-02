4f9e2f8ff78822005d4793d9bf754a518e04571e
Alexander Barabash <alexanderbarabash86@gmail.com>
2022-05-02 16:53:30 +0300

    Add test model & dm

--- RandomIdea/ViewControllers/OptionCell.swift
+++ RandomIdea/ViewControllers/OptionCell.swift
@@ -10,46 +10,54 @@ import UIKit
 class OptionCell: UITableViewCell {
     
     var iconOptionLabel = UILabel()
-    var infoOptionLabel = UILabel()
+    var titleOptionLabel = UILabel()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         addSubview(iconOptionLabel)
-        addSubview(infoOptionLabel)
+        addSubview(titleOptionLabel)
         
         configureIconOptionLabel()
         configureInfoOptionLabel()
         setIconOptionConstraints()
-        setInfoOptionConstraints()
+        setTitleOptionConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func configureIconOptionLabel() {
-        infoOptionLabel.numberOfLines = 0
+    func set(idea: Idea) {
+        iconOptionLabel.text = idea.icon
+        titleOptionLabel.text = idea.title
     }
     
-    private func configureInfoOptionLabel() {
-        infoOptionLabel.numberOfLines = 0
-        infoOptionLabel.adjustsFontSizeToFitWidth = true
+    func configureIconOptionLabel() {
+        titleOptionLabel.numberOfLines = 0
     }
     
-    private func setIconOptionConstraints() {
+    func configureInfoOptionLabel() {
+        titleOptionLabel.numberOfLines = 0
+        titleOptionLabel.adjustsFontSizeToFitWidth = true
+    }
+    
+    func setIconOptionConstraints() {
         iconOptionLabel.translatesAutoresizingMaskIntoConstraints = false
         iconOptionLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         iconOptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20).isActive = true
         iconOptionLabel.heightAnchor.constraint(equalToConstant: 80).isActive = true
         iconOptionLabel.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        iconOptionLabel.font = UIFont(name: iconOptionLabel.font.fontName, size: 50)
     }
     
-    private func setInfoOptionConstraints() {
-        iconOptionLabel.translatesAutoresizingMaskIntoConstraints = false
-        iconOptionLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-        infoOptionLabel.leadingAnchor.constraint(equalTo: iconOptionLabel.trailingAnchor, constant: 20).isActive = true
-        iconOptionLabel.heightAnchor.constraint(equalToConstant: 80).isActive = true
-        iconOptionLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12).isActive = true
+    func setTitleOptionConstraints() {
+        titleOptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleOptionLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        titleOptionLabel.leadingAnchor.constraint(equalTo: iconOptionLabel.trailingAnchor, constant: 20).isActive = true
+        titleOptionLabel.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        titleOptionLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12).isActive = true
+//        titleOptionLabel.font = UIFont(name: titleOptionLabel.font.fontName, size: 25)
+        titleOptionLabel.font = UIFont.boldSystemFont(ofSize: 25)
     }
     
 }
