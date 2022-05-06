b7a2f74395ec23c935ba3b0aef46aad7fdf80752
Alexander Barabash <alexanderbarabash86@gmail.com>
2022-05-06 15:31:58 +0300

    Add alert and more info

---
+++ RandomIdea/Extensions-Utilities/MyListCell.swift
@@ -0,0 +1,55 @@
+//
+//  MyListCell.swift
+//  RandomIdea
+//
+//  Created by Alexandr Barabash on 06.05.2022.
+//
+
+import UIKit
+
+class MyListCell: UITableViewCell {
+    
+    
+    var myListLabel = UILabel()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubview(myListLabel)
+        
+        configureMyLisLabel()
+        configureInfoMyListLabel()
+        setTitleOptionConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+// доделать!!!
+//    func set(_: String) {
+//        myListLabel.text = String? ?? ""
+//    }
+    
+    func configureMyLisLabel() {
+        myListLabel.numberOfLines = 0
+    }
+    
+    func configureInfoMyListLabel() {
+        myListLabel.numberOfLines = 0
+        myListLabel.adjustsFontSizeToFitWidth = true
+    }
+    
+
+    
+    func setTitleOptionConstraints() {
+        myListLabel.translatesAutoresizingMaskIntoConstraints = false
+        myListLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        myListLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12).isActive = true
+        myListLabel.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        myListLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12).isActive = true
+//        myListLabel.font = UIFont(name: myListLabel.font.fontName, size: 25)
+        myListLabel.font = UIFont.boldSystemFont(ofSize: 25)
+    }
+//
+}
+
